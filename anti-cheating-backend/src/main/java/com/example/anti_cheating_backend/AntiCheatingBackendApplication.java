c023d9766c2911441775af89ce1a592dc30cb5b1
Janith-D <janetdgo2001@gmail.com>
2025-09-28 00:58:39 +0530

    first commit

---
+++ anti-cheating-backend/src/main/java/com/example/anti_cheating_backend/AntiCheatingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.anti_cheating_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiCheatingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiCheatingBackendApplication.class, args);
+	}
+
+}
---
+++ anti-cheating-backend/src/test/java/com/example/anti_cheating_backend/AntiCheatingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anti_cheating_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiCheatingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
